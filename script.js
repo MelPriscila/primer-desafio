7ece28de4f6eeeaffe44f2e0dfedbc5cf65b7cd6
Melanie Lema <melanielema1805@gmail.com>
2022-07-27 20:09:28 -0300

    Funciones

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let precio = 100
+/*let precio = 100
 let repetir = true
 
 while (repetir) {
@@ -12,4 +12,32 @@ while (repetir) {
         repetir = false
         alert("!Gracias por su compra!")
     }
+}
+*/
+let respuesta
+
+do {
+    const precio = 100
+
+    function multiplicacion(numero1 = 1, precio) {
+        return numero1 * precio
+    }
+
+    do {
+        numero1 = parseInt(prompt("¡Bienvenido! Ingrese la cantidad de combos que desea, el precio es de $100 por cada uno"))
+
+        if (isNaN(numero1)) {
+            alert("Por favor ingrese un numero, no letras")
+        } else {
+            alert(`El valor de su pedido es igual a $${numero1 * precio}`)
+        }
+
+        let resultado = multiplicacion(numero1, precio)
+
+    } while (isNaN(numero1))
+
+    respuesta = prompt("Desea hacer otro pedido?").toLowerCase()
+
+} while (respuesta != "no") {
+    alert("¡Gracias por su compra!")
 }
